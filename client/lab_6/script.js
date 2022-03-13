9ce04cc6d58d5b454801fc50b3a306c67dd4a3f7
yang <ylk01413@gmail.com>
2022-03-13 19:31:10 -0400

    lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,15 +1,50 @@
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
-  });
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
 }
 
+function restArrayMake(dataArray) {
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+    return dataArray[restNum];
+  });
+  console.log(listItems);
+}
+
+function createHtmlList(collection) {
+  const targetList = document.querySelector('.restList');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const { name } = item;
+    const displayName = name.toLowerCase();
+    const injectThis = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThis;
+  });
+}
+async function mainEvent() {
+  // the async keyword means we can make API requests
+  const form = document.querySelector('.main_form');
+  const submitButton = document.querySelector('#submit_button');
+  submitButton.style.display = 'none';
+
+  const results = await fetch(
+    'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+  ); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+
+  if (arrayFromJson.length > 0) {
+    submit_button.style.display = 'block';
+    form.addEventListener('submit', async (submitEvent) => {
+      // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      console.log('form submission'); // this is substituting for a 'breakpoint'
+      const restArray = restArrayMake(arrayFromJson.data);
+      createHtmlList(restArray);
+    });
+  }
+}
+restArrayMake();
 // this actually runs first! It's calling the function above
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
